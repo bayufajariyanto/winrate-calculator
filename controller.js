d02cc4cd09620ec19f2878fa625882944800ecd5
Bayu Fajariyanto <bayuf08@gmail.com>
2021-06-14 12:39:01 +0700

    loader

--- controller.js
+++ controller.js
@@ -3,6 +3,7 @@ app.controller('winrateController', ['$scope', ($scope) =>{
     $scope.title = 'Float Labs'
     $scope.match = $scope.winrate = $scope.target = 0
     $scope.pernyataan = ""
+    $scope.loading = false
     $scope.submit = () => {
         if($scope.match <= 0 ||$scope.winrate <= 0 || $scope.target <= 0 ||
             $scope.match == null || $scope.match == undefined ||
@@ -18,19 +19,16 @@ app.controller('winrateController', ['$scope', ($scope) =>{
             wr = (menang*100)/match
             if($scope.target >= $scope.winrate){                
                 while(wr < $scope.target){
+                    $scope.loading = true
                     wr = ((menang++) * 100)/(match++)
                     wr = wr.toFixed(2)
-                    console.log({
-                        menang: menang,
-                        match: match,
-                        wr: wr
-                    })
                 }
                 hasil = match-$scope.match
                 $scope.pernyataan = "Kamu harus menang <b>" + hasil + " kali</b> tanpa kalah untuk mendapatkan winrate " + wr + "%"
             }else{
                 $scope.pernyataan = "Target harus lebih besar dari winrate"
             }
+            $scope.loading = false
         }
     }
 }])
