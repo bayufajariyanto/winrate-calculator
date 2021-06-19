f696e2101939c73daea8e7c526c6f6bfca471b78
Bayu Fajariyanto <bayuf08@gmail.com>
2021-06-19 10:10:42 +0700

    statistik

--- controller.js
+++ controller.js
@@ -14,9 +14,14 @@ app.controller('winrateController', ['$scope', ($scope) =>{
             let menang = kalah = wr = 0
             target = $scope.target.toFixed(2)
             match = $scope.match.toFixed(2)
-            menang = Math.floor(($scope.match * $scope.winrate) / 100)
-            kalah = Math.floor($scope.match - menang)
+            menang1 = menang = Math.floor(($scope.match * $scope.winrate) / 100)
+            kalah1 = kalah = Math.floor($scope.match - menang)
             wr = (menang*100)/match
+            console.log({
+                wr: wr,
+                menang: menang,
+                kalah: kalah
+            })
             if($scope.target >= $scope.winrate){                
                 while(wr < $scope.target){
                     $scope.loading = true
@@ -24,7 +29,7 @@ app.controller('winrateController', ['$scope', ($scope) =>{
                     wr = wr.toFixed(2)
                 }
                 hasil = match-$scope.match
-                $scope.pernyataan = "Kamu harus menang <b>" + hasil + " kali</b> tanpa kalah untuk mendapatkan winrate " + wr + "%"
+                $scope.pernyataan = "Statistik kamu:<br><ul><li>Menang : "+menang1+"</li><li>Kalah : "+kalah1+"</li></ul><br>Kamu harus menang <b>" + hasil + " kali</b> tanpa kalah untuk mendapatkan winrate " + wr + "%"
             }else{
                 $scope.pernyataan = "Target harus lebih besar dari winrate"
             }
